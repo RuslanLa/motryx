d87d1462746aa1c4b4bf3994527bfd75498c116d
RuslanLatypov <ruslan.latypov@akvelon.com>
2018-12-16 18:18:32 +0300

    implemented more tests

--- src/__tests__/observable.tests.ts
+++ src/__tests__/observable.tests.ts
@@ -3,29 +3,63 @@ import { autoRun } from "../observable";
 
 let monkey = new Monkey();
 const someAction = jest.fn();
+const autorunCounter = jest.fn();
 beforeEach(() => {
     monkey = new Monkey();
     someAction.mockClear();
+    autorunCounter.mockClear();
 });
 describe('autorun tests', () => {
     test('should run firstly on subscription', () => {
         autoRun(() => {
             someAction(monkey.fullness);
+            autorunCounter();
 
         });
-        expect(someAction).toBeCalledTimes(1);
+        expect(autorunCounter).toBeCalledTimes(1);
     });
     test('should run exactly same count as observable fields changed', () => {
-        monkey = new Monkey();
         autoRun(() => {
             someAction(monkey.fullness);
+            autorunCounter();
         });
+        autorunCounter.mockClear();
         monkey.fullness = 100;
-        expect(someAction).toBeCalledTimes(2);
+        expect(autorunCounter).toBeCalledTimes(1);
         monkey.fullness = 100;
-        expect(someAction).toBeCalledTimes(3);
+        expect(autorunCounter).toBeCalledTimes(2);
         monkey.fullness = 10;
         monkey.fullness = 4;
-        expect(someAction).toBeCalledTimes(5);
-    })
+        expect(autorunCounter).toBeCalledTimes(4);
+    });
+    test('should subscribe several fields', () => {
+        autoRun(() => {
+            someAction(monkey.fullness);
+            someAction(monkey.health);
+            autorunCounter();
+        });
+        autorunCounter.mockClear();
+        monkey.fullness = 100;
+        monkey.health = 100;
+        expect(autorunCounter).toBeCalledTimes(2);
+    });
+    test('should not affect not subscribed fields', () => {
+        autoRun(() => {
+            someAction(monkey.fullness);
+            autorunCounter();
+        });
+        autorunCounter.mockClear();
+        monkey.health = 100;
+        expect(autorunCounter).not.toBeCalled();
+    });
+    test('should run once changes in action', () => {
+        autoRun(() => {
+            someAction(monkey.fullness);
+            someAction(monkey.health);
+            autorunCounter();
+        });
+        autorunCounter.mockClear();
+        monkey.feed({ calorie: 20, effect: -10 });
+        expect(autorunCounter).toBeCalledTimes(1);
+    });
 });
